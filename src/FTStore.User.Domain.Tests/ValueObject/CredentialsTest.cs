6e44f66d625ab34bac6e3a6d983170c54708e341
Francisco Thiago <ftathiago@gmail.com>
2020-04-05 22:13:10 -0300

    Create first testes do Credentials

--- src/FTStore.User.Domain.Tests/ValueObject/CredentialsTest.cs
+++ src/FTStore.User.Domain.Tests/ValueObject/CredentialsTest.cs
@@ -1,17 +1,33 @@
+using FluentAssertions;
+using FTStore.User.Domain.ValueObjects;
 using Xunit;
 
 namespace FTStore.User.Domain.Tests.ValueObject
 {
     public class CredentialsTest
     {
+        private const string SECRET_PHRASE = "swordfish";
+        private const string EMAIL = "admin@admin.com";
+
         [Fact]
-        public void Should()
+        public void ShouldCreateWithEMailAndSecretPhrase()
         {
-            //Given
+            var credentials = new Credentials(EMAIL, SECRET_PHRASE);
 
-            //When
+            credentials.Should().NotBeNull();
+            credentials.Email.Should().Be(EMAIL);
+            credentials.Password.Should().NotBeNull();
+        }
 
-            //Then
+        [Fact]
+        public void ShouldCreateWithEmailAndPassword()
+        {
+            var password = new Password(SECRET_PHRASE);
+
+            var credentials = new Credentials(EMAIL, password);
+
+            credentials.Email.Should().Be(EMAIL);
+            credentials.Password.Should().Be(password);
         }
     }
 }
