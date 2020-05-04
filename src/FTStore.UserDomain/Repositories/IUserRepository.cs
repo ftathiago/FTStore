8fecda478eeb23feb457163dad229e1d73ba946d
Francisco Thiago <ftathiago@gmail.com>
2020-05-04 10:05:02 -0300

    Refactor: repository does not generate credentials

--- src/FTStore.UserDomain/Repositories/IUserRepository.cs
+++ src/FTStore.UserDomain/Repositories/IUserRepository.cs
@@ -6,9 +6,6 @@ namespace FTStore.UserDomain.Repositories
 {
     public interface IUserRepository
     {
-        Credentials GetCredentialsBy(string email);
         User GetByEmail(string email);
-
-        IEnumerable<string> GetUserClaims(int id);
     }
 }
