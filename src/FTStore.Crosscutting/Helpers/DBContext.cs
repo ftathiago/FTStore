850325ec1e02d705efdf036fb0bcdc38870b22c8
Francisco Thiago <ftathiago@gmail.com>
2020-05-10 20:59:30 -0300

    Add Migrate to crosscuting

--- src/FTStore.Crosscutting/Helpers/DBContext.cs
+++ src/FTStore.Crosscutting/Helpers/DBContext.cs
@@ -1,7 +1,11 @@
 using System;
 using FTStore.Infra.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using FTStore.Crosscutting.Model;
+using MySql.Data.MySqlClient;
 
 namespace FTStore.Crosscutting.Helpers
 {
@@ -28,5 +32,47 @@ namespace FTStore.Crosscutting.Helpers
             );
             return services;
         }
+
+        public static string BuildConnectionString(this IConfiguration configuration)
+        {
+            var appSettings = GetAppSettings(configuration);
+            var builder = new MySqlConnectionStringBuilder(configuration.GetConnectionString("FTStoreDB"));
+            builder.Database = "FTStore";
+            if (!string.IsNullOrEmpty(appSettings.DBHost))
+                builder.Server = appSettings.DBHost;
+            if (!string.IsNullOrWhiteSpace(appSettings.DBUsuario))
+                builder.UserID = appSettings.DBUsuario;
+            if (!string.IsNullOrWhiteSpace(appSettings.DBSenha))
+                builder.Password = appSettings.DBSenha;
+            var connectionString = builder.ConnectionString;
+            return connectionString;
+        }
+
+        private static AppSettings GetAppSettings(IConfiguration configuration)
+        {
+            var appSettings = new AppSettings();
+            appSettings.DBHost = configuration["DBHOST"] ?? configuration["DatabaseConfig:DatabaseHost"];
+            appSettings.DBUsuario = configuration["USERID"] ?? configuration["DatabaseConfig:UserID"];
+            appSettings.DBSenha = configuration["USERPASS"] ?? configuration["DatabaseConfig:UserPass"];
+            return appSettings;
+        }
+
+        public static IHost InitializeDataBase(this IHost host)
+        {
+            var serviceScopeFactory = host.Services;
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+
+                FTStoreDbContext dbContext = services.GetRequiredService<FTStoreDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            return host;
+        }
+
+
     }
 }
