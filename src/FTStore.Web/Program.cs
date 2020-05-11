84fe5d2ad5fa3dd101ea9fde0d5004f191be4d40
Francisco Thiago <ftathiago@gmail.com>
2020-05-10 21:07:08 -0300

    Add database initialization

--- src/FTStore.Web/Program.cs
+++ src/FTStore.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using FTStore.Crosscutting.Helpers;
 
 namespace FTStore.Web
 {
@@ -7,7 +8,10 @@ namespace FTStore.Web
     {
         public static void Main(string[] args)//NOSONAR
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .InitializeDataBase()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
