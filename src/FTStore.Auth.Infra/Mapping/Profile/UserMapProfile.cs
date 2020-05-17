a996789223e5e9c2a34218097eff28cde5be7078
Francisco Thiago <ftathiago@gmail.com>
2020-05-17 17:34:42 -0300

    Change mapping to User

--- src/FTStore.Auth.Infra/Mapping/Profile/UserMapProfile.cs
+++ src/FTStore.Auth.Infra/Mapping/Profile/UserMapProfile.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using AutoMapper;
 using FTStore.Auth.Domain.Entities;
 using FTStore.Auth.Infra.Tables;
@@ -8,8 +10,20 @@ namespace FTStore.Auth.Infra.Mappings.Profiles
     {
         public UserMapProfile()
         {
-            CreateMap<User, UserTable>();
-            CreateMap<UserTable, User>();
+            CreateMap<User, UserTable>()
+                .ForMember(
+                    userTable => userTable.IsAdmin,
+                    c => c.MapFrom(m => m.Claims.Contains("admin")
+                )
+            );
+            CreateMap<UserTable, User>()
+                .ForMember(
+                    user => user.Claims,
+                    opt => opt.MapFrom(
+                        userTable => userTable.IsAdmin ?
+                            new List<string> { "admin" }
+                            : new List<string>())
+                );
         }
     }
 }
