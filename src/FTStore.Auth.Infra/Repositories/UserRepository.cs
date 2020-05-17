f03ed7ded5662248c1726e2632614b0c2d4bbc28
Francisco Thiago <ftathiago@gmail.com>
2020-05-17 17:35:13 -0300

    Write a real code to UserRepository

--- src/FTStore.Auth.Infra/Repositories/UserRepository.cs
+++ src/FTStore.Auth.Infra/Repositories/UserRepository.cs
@@ -1,17 +1,24 @@
+using System.Linq;
+
+using AutoMapper;
+
 using FTStore.Auth.Domain.Entities;
 using FTStore.Auth.Domain.Repositories;
-using FTStore.Auth.Domain.ValueObjects;
+using FTStore.Auth.Infra.Tables;
+using FTStore.Infra.Context;
+using FTStore.Lib.Common.Repositories;
 
 namespace FTStore.Auth.Infra.Repositories
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : BaseRepository<User, UserTable>, IUserRepository
     {
+        public UserRepository(FTStoreAuthContext ftStoreAuthContext, IMapper mapper)
+          : base(ftStoreAuthContext, mapper) { }
         public User GetByEmail(string email)
         {
-            var password = new Password("swordfish");
-            var user = new User("user name", "surname", "admin@admin.com", password);
-            user.Claims.Add("Teste");
-            return user;
+            var user = DbSet.Where(user => user.Email == email).FirstOrDefault();
+            User userEntity = _mapper.Map<UserTable, User>(user);
+            return userEntity;
         }
     }
 }
